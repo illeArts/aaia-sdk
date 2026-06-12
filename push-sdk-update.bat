56554284a19ed3a83525194ebaf90ce6f224a446
illeArts <illedmx@gmail.com>
2026-06-12 17:47:31 +0200

    fix: suppress CS1587 + add .gitattributes

--- push-sdk-update.bat
+++ push-sdk-update.bat
@@ -1,26 +1,8 @@
 @echo off
 chcp 65001 >nul
 cd /d "H:\AAIAGitHub\aaia-sdk"
-
-echo.
-echo === aaia-sdk - Commit + Push ===
-echo.
-
 git add .
-
-git commit -m "feat: Publisher + Security contracts; Marketplace DTOs erweitert
-
-- Publisher/ Namespace hinzugefuegt (RegisterPublisherKeyRequest/Response, PublisherKeyDto etc.)
-- Security/ Namespace hinzugefuegt
-- MarketplaceDtos: DeveloperLoginRequest/Response, DeveloperRegisterRequest/Response, DeveloperAccountDto
-- ExtensionManifestDtos: aktualisiert
-- AaiaApiRoutes: aktualisiert
-- release-contracts scripts hinzugefuegt"
-
-echo.
-echo Pushing to GitHub...
+git commit -m "fix: suppress CS1587 + add .gitattributes"
 git push origin main
-
-echo.
 echo === Done! ===
 pause
